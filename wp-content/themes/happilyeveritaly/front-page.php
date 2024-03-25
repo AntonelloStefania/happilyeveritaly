c27c1ee91344f11249b6091fdcd7aae563dd5cff
Antonello <antonelloste94@gmail.com>
2024-03-25 22:08:33 +0100

    copiati files tema padre in tema figlio header/front-page

---
+++ wp-content/themes/happilyeveritaly/front-page.php
@@ -0,0 +1,14 @@
+
+<?php 
+get_header(); ?>
+
+<div class="container mt-5 pt-5">
+    <div class="row mt-5">
+        <div class="col-12 mt-5">
+            <h1>hello</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis dolor officiis vero libero, doloremque tenetur quis nesciunt esse, nemo sequi provident laboriosam nisi, quisquam mollitia reiciendis laudantium asperiores ipsa minima! Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae nihil iure ipsum? Cumque, laboriosam eius ea iste in id quis qui repellendus vero quaerat voluptas nostrum quo culpa? Ea, tempora!</p>
+        </div>
+    </div>
+</div>
+<?php
+get_footer();
--- wp-content/themes/happilyeveritaly/functions.php
+++ wp-content/themes/happilyeveritaly/functions.php
@@ -1,5 +1,5 @@
 <?php 
-
+// css
 function happilyeveritaly_styles() {
     wp_enqueue_style( 
         'happily-ever-italy', 
@@ -11,3 +11,11 @@ function happilyeveritaly_styles() {
     );
 }
 add_action( 'wp_enqueue_scripts', 'happilyeveritaly_styles' );
+
+// bootstrap
+function enqueue_bootstrap() {
+    wp_enqueue_style('bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css', array(), '4.5.2', 'all');
+    wp_enqueue_script('bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js', array('jquery'), '4.5.2', true);
+}
+
+add_action('wp_enqueue_scripts', 'enqueue_bootstrap');
